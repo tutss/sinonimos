6d821ef682eb4614d72ef50492539b11a34832c6
tutss <artur_santos@usp.br>
2019-11-10 13:44:28 -0300

    Added meaning to found words

--- sinonimos.py
+++ sinonimos.py
@@ -12,16 +12,24 @@ url = "https://www.sinonimos.com.br/" + word_to_search
 request_html = requests.get(url)
 soup = bs4.BeautifulSoup(request_html.text, "html.parser")
 
-related_words = []
+meaning_and_words = dict()
 
 wrappers = soup.findAll("div", {CLASS: "s-wrapper"})
 for wrapper in wrappers:
+    related_words = []
+    
+    meaning = wrapper.find_next("div", {CLASS: "sentido"})
     synonms = wrapper.findAll("a", {CLASS: "sinonimo"})
     for synon in synonms:
         related_words.append(synon.get_text())
+    
+    meaning_and_words[meaning.get_text()[:-1]] = related_words
 
-print("Related words:")
-for word in related_words:
-    print("  --> ", word)
+print("====== Palavras relacionadas ======")
+for mean in meaning_and_words.keys():
+    print("{}:".format(mean))
+    words = meaning_and_words[mean]
+    for word in words:
+        print(" ----> {}".format(word))
 
 
